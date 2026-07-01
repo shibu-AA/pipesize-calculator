7a40689036264268fd02dab7f045e926f1d59eae
shibu-AA <genki.shibuya2000@gmail.com>
2026-07-01 15:53:14 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,127 @@
+import streamlit as st
+from calc.calculator import select_pipe
+
+input_data = {
+    "gas": None,
+    "flow_rate": None,
+    "inlet_pressure": None,
+    "outlet_pressure": None,
+    "temperature": None,
+    "velocity_limit": None,
+    "schedule": None,
+    "pipe_length": None
+}
+
+fitting_names = [
+    "90°エルボ(2･1/2まで)",
+    "90°エルボ(3\"～6\")",
+    "90°ベント",
+    "45°エルボ",
+    "チーズ",
+    "弁(2･1/2まで)",
+    "弁(3\"～6\")"
+]
+
+st.set_page_config(
+    page_title="配管サイズ自動算定",
+    page_icon="🛠️",
+    layout="centered"
+)
+
+st.title("配管サイズ自動算定")
+
+st.markdown("### 入力条件")
+
+# ① ガス名
+gas = st.selectbox(
+    "① ガス名",
+    ["Air"]
+)
+
+# ② 流量
+flow_rate = st.number_input(
+    "② 流量 (Nm³/h)",
+    min_value=0.0,
+    value=1000.0,
+    step=10.0
+)
+
+# ③ 入口圧力
+inlet_pressure = st.number_input(
+    "③ 入口圧力 (MPa)",
+    min_value=0.0,
+    value=0.9,
+    step=0.1
+)
+
+# ④ 出口圧力
+outlet_pressure = st.number_input(
+    "④ 出口圧力 (MPa)",
+    min_value=0.0,
+    value=0.7,
+    step=0.1
+)
+
+# ⑤ 基準温度
+temperature = st.number_input(
+    "⑤ 基準温度 (℃)",
+    value=20.0,
+    step=1.0
+)
+
+# ⑥ 許容流速
+velocity_limit = st.number_input(
+    "⑥ 許容流速 (m/s)",
+    min_value=0.0,
+    value=8.0,
+    step=0.5
+)
+
+# ⑦ 配管規格
+schedule = st.selectbox(
+    "⑦ 配管規格",
+    ["BA", "sch5", "sch10"]
+)
+
+# ⑧ 管の長さ
+pipe_length = st.number_input(
+    "⑧ 管の長さ (mm)",
+    min_value=0.0,
+    value=1000.0,
+    step=100.0
+)
+
+st.markdown("### 継手入力")
+
+# 継ぎ手の数量
+fitting_counts = []
+
+for i, name in enumerate(fitting_names):
+    val = st.number_input(
+        name,
+        min_value=0,
+        step=1,
+        value=0,
+        key=i
+    )
+    fitting_counts.append(val)
+
+# 計算ボタン
+if st.button("計算"):
+
+    input_data = {
+        "gas": gas,
+        "flow_rate": flow_rate,
+        "inlet_pressure": inlet_pressure,
+        "outlet_pressure": outlet_pressure,
+        "temperature": temperature,
+        "velocity_limit": velocity_limit,
+        "schedule": schedule,
+        "pipe_length": pipe_length,
+        "fitting_counts": fitting_counts
+    }
+
+    pipe_name = select_pipe(input_data)
+
+    st.write("### 推奨配管サイズ")
+    st.success(pipe_name)
---
+++ calc/calculator.py
@@ -0,0 +1,47 @@
+import math
+import pandas as pd
+
+PIPE_FILES = {
+    "BA": "data/pipe/BA.csv",
+    "sch5": "data/pipe/sch5.csv",
+    "sch10": "data/pipe/sch10.csv"
+}
+
+fitting_equivalent_lengths = [30, 40, 15, 18, 60, 45, 60]
+
+def select_pipe(input_data):
+
+    gas = input_data["gas"]
+    flow_rate = input_data["flow_rate"]
+    inlet_pressure = input_data["inlet_pressure"]
+    outlet_pressure = input_data["outlet_pressure"]
+    temperature = input_data["temperature"]
+    velocity_limit = input_data["velocity_limit"]
+    schedule = input_data["schedule"]
+    pipe_length = input_data["pipe_length"]
+    fitting_counts = input_data["fitting_counts"]
+
+    gas_table = pd.read_csv("data/gas/properties.csv")
+    M = gas_table.loc[
+        gas_table["化学式"] == gas,
+        "分子量(Kg/Kmol)"
+    ].iloc[0]
+
+    actual_flow = (flow_rate/(1000*60))*(101.3/(101.3+outlet_pressure*1000))*((273+temperature)/273)
+    required_diameter = math.sqrt((4 * actual_flow) / (math.pi * velocity_limit)) * 1000
+
+    pipe_table = pd.read_csv(PIPE_FILES[schedule])
+
+    filtered = pipe_table[
+        pipe_table["内径(D)(mm)"] >= required_diameter
+    ]
+
+    for _, row in filtered.iterrows():
+        fluid_density = ((inlet_pressure*10.1972+1.033)/1.033*101325)/(8314.3*293/M)
+        n = sum([fitting_counts[i] * fitting_equivalent_lengths[i] for i in range(len(fitting_counts))])
+        Ln = row["内径(D)(mm)"] * n / 1000
+        delta_P = (((row["摩擦係数"])*(pipe_length+Ln)*fluid_density)*(velocity_limit**2))/(row["内径(D)(mm)"]*0.001*2*9.8)*0.0001
+
+        if outlet_pressure < inlet_pressure - delta_P*0.0980665:
+            return row["呼び径"]
+    return "ERROR"
